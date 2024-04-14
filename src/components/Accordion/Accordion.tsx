3e5556ee356ec278cb68d70ba1f4b560ee572946
Michael Young <neuromyoung@gmail.com>
2024-04-13 20:21:18 -0400

    add mongoose

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -12,31 +12,31 @@ const Accordion = () => {
         {
             item: "Greeting cards",
             description: "Share artwork with friends and loved ones with these vibrant greeting cards.",
-            imgsrc: "/static/images/store/cards.jpg",
+            imgsrc: "/store/cards.jpg",
             alt: "photo of a little critter card pack."
         },
         {
             item: "Coaster Sets",
             description: "Unique and eye-popping coaster sets each with a curated collection of four drawings.",
-            imgsrc: "/static/images/store/coasters.jpg",
+            imgsrc: "/store/coasters.jpg",
             alt: "photo of a bird coaster set."
         },
         {
             item: "Prints",
             description: "High-quality prints of colored pencil drawings are the perfect addition to any home.",
-            imgsrc: "/static/images/store/prints.jpg",
+            imgsrc: "/store/prints.jpg",
             alt: "photo of three example prints made from colored pencil art"
         },
         {
             item: "Original Art",
             description: "Original, one-of-a-kind, colored pencil art.",
-            imgsrc: "/static/images/store/original.jpg",
+            imgsrc: "/store/original.jpg",
             alt: "photo of an original colored pencil wildlife drawing."
         },
         {
             item: "Commission",
             description: "Work directly with me to create your own custom colored pencil portrait.",
-            imgsrc: "/static/images/store/portrait.jpg",
+            imgsrc: "/store/portrait.jpg",
             alt: "time lapse photo of a dog portrait."
         }
     ]
--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -1,49 +1,22 @@
 
 import Image from 'next/image'
-import styles from './shop.module.css'
 import Accordion from '../Accordion/Accordion'
 
 const Shop = () => {
-    return(
-        <section className={styles.shopWrapper}>
-            <div className={styles.shopContainer}>
-                <div className={styles.content}>
+    return (
+        <section className="w-full bg-[#f2f2f2] min-h-screen flex justify-center">
+            <div className="w-full max-w-[1600px] grid grid-cols-2 grid-rows-1 gap-5">
+                <div className="h-full flex flex-col justify-center gap-10 p-[1rem] relative items-around bg-red-100">
                     <Accordion />
-
-                    <button className={styles.storeButton}>
+                    <button className="relative w-[200px] transition-all ease-in-out duration-200  text-[#0e0008] text-[1.2rem] py-[0.75rem] px-[2rem] bg-[#000] bg-opacity-0 pointer rounded-sm outline outline-1 outline-[#0e0008] hover:bg-opacity-20 focus:bg-opacity-20 hover:scale-105 focus:scale-105">
                         Visit Store
                     </button>
                 </div>
-                <div className={styles.Outer}>  
-                        <div className={styles.imgWrapper}> 
-                            <Image
-                                src="/static/images/fireplace_mockup_small.png"
-                                fill={true}
-                                style={{objectFit:"cover"}}
-                                alt="Colored pencil drawing of a short-haired pointer."
-                            />
-                        </div>
-                        <div className={styles.Inner}>
-                            <div className={styles.imgWrapper}>
-                                <Image
-                                    src="/static/images/panther-mockup_compressed_small.png"
-                                    fill={true}
-                                    style={{objectFit:"cover"}}
-                                    alt="Colored pencil drawing of a short-haired pointer."
-                                />
-                            </div>
-                            <div className={styles.imgWrapper}>
-                                <Image
-                                    src="/static/images/hummingbird_mockup_small.png"
-                                    fill={true}
-                                    style={{objectFit:"cover"}}
-                                    alt="Colored pencil drawing of a short-haired pointer."
-                                />
-                            </div>
-                        </div>
+                <div className="bg-green-100">
+
                 </div>
             </div>
-        </section>
+        </section >
     )
 }
 
