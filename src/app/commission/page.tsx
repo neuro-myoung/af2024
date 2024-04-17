126e3e25fa645c7e66756a816faca167bcf7dee5
Michael Young <neuromyoung@gmail.com>
2024-04-16 23:25:56 -0400

    remove unescaped entities

--- src/app/commission/page.tsx
+++ src/app/commission/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
-import PriceTable from '@/components/PriceTable/PriceTable'
+import Image from 'next/image';
+import PriceTable from '@/components/PriceTable/PriceTable';
 
 const Commission = () => {
     return (
         <main className="flex flex-col text-[#0e0008] w-screen min-h-screen justify-center items-center bg-[#f2f2f2] px-12 py-64">
-            <h1 className="max-w-[1600px] w-full">Commission Artwork</h1>
-            <div className="relative max-w-[1600px] w-full before:content-[''] before:absolute before:w-[100%] before:h-[20px] before:l-0 before:t-0 before:border-l-2 before:border-r-2 before:border-t-2 before:border-[#0e0008] after:content-[''] after:absolute after:w-[100%] after:h-[20px] after:r-0 after:b-100 after:border-l-2 after:border-r-2 after:border-b-2 after:border-[#0e0008]">
+            <h1 className="max-w-[1400px] w-full">Commission Artwork</h1>
+            <div className="relative max-w-[1400px] w-full before:content-[''] before:absolute before:w-[100%] before:h-[20px] before:l-0 before:t-0 before:border-l-2 before:border-r-2 before:border-t-2 before:border-[#0e0008] after:content-[''] after:absolute after:w-[100%] after:h-[20px] after:r-0 after:b-100 after:border-l-2 after:border-r-2 after:border-b-2 after:border-[#0e0008]">
                 <div className="flex px-8 gap-12">
                     <div className="w-full py-12 px-6 flex gap-6">
                         <div className="flex flex-col gap-8">
@@ -32,7 +32,7 @@ const Commission = () => {
                     <h2 className="text-[2rem] font-semibold"><span className="text-[#e93e68] font-semibold">High quality references</span> help capture the details</h2>
                     <p className="max-w-[60ch] text-[1.2rem] my-[0.75rem]">
                         The most suitable reference photos for portraiture are taken up close at eye level 
-                        and lit with natural light. If you can't decide, I'm happy to give suggestions. 
+                        and lit with natural light. If you can&apos;t decide, I&apos;m happy to give suggestions. 
                         You can submit multiple photos that best represent distinct aspects you would like to capture.
                         Photos can be sent by email or by postal mail.  
                     </p>
